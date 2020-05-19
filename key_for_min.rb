2dea822f56c947c4b88593b8914fc11b4b7b0f42
Brad Carter <bradc1993@gmail.com>
2020-05-19 23:16:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,10 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
-  name_hash.each do 
+  name_hash.each do |k, v|
+    if v < lowest_value
+      v = lowest_value
+      k = lowest_key
+    end
 
 end
